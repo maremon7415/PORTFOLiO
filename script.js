872019e7f70283e428ee6ae2c700504836f5fa78
maremon7415 <maremon15@gmail.com>
2024-08-29 20:49:52 +0600

    SomeColorGrading

---
+++ script.js
@@ -0,0 +1,100 @@
+// animatedText//
+function animatedText() {
+  let words = document.querySelectorAll(".word");
+  words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+      let span = document.createElement("span");
+      span.textContent = letter;
+      span.className = "letter";
+      word.append(span);
+    });
+  });
+
+  let currentWordIndex = 0;
+  let maxWordIndex = words.length - 1;
+  words[currentWordIndex].style.opacity = "1";
+
+  let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord =
+      currentWordIndex === maxWordIndex
+        ? words[0]
+        : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+      setTimeout(() => {
+        letter.className = "letter out";
+      }, i * 80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+      letter.className = "letter behind";
+      setTimeout(() => {
+        letter.className = "letter in";
+      }, 340 + i * 80);
+    });
+    currentWordIndex =
+      currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+  };
+  changeText();
+  setInterval(changeText, 3000);
+}
+animatedText();
+
+function loaderCirle() {
+  const circles = document.querySelectorAll(".circle");
+  circles.forEach((elem) => {
+    var dots = parseInt(elem.getAttribute("data-dots"));
+    var marked = parseInt(elem.getAttribute("data-parcent"));
+    var parcent = Math.floor((dots * marked) / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+      points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll(".points");
+    for (let i = 0; i < parcent; i++) {
+      pointsMarked[i].classList.add("marked");
+    }
+  });
+}
+loaderCirle();
+
+// mixItUP//
+
+var mixer = mixitup(".portfolio-gallery");
+
+// ActiveManu//
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  menuLi.forEach((sec) => sec.classList.remove("active"));
+  menuLi[len].classList.add("active");
+}
+activeMenu();
+
+// stickyNav//
+
+function stickyNav() {
+  window.addEventListener("scroll", activeMenu);
+
+  const header = document.querySelector("header");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+}
+stickyNav();
